ff6edcfd6dbaa494eed4d4bd570505e06a1c7008
chandan yadav <chandanky.23@gmail.com>
2020-11-01 18:33:31 +0100

    Adding the euclidean distance helper method, calculating the accuracy for the labels classified for various values of k ,i.e, nearest samples.

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -2,6 +2,9 @@
 # @description: A sample is classified by a popularity vote by its neighbours.
 
 # to calculate the distance between neighbours we use Euclidean Distance
+import numpy as np
+from collections import Counter
+from euclidea_distance import euclidean_distance
 
 class KNN:
   
@@ -10,7 +13,24 @@ class KNN:
     self.k = k
 
   def fit(self, X, y):
-    pass
+    self.X_train = X
+    self.y_train = y
 
   def predict(self, X):
-    pass
+    predicted_labels = [self._predict(x) for x in X]
+    return np.array(predicted_labels)
+
+  def _predict(self, x):
+    # compute the distances (Euclidean distance)
+    distanes = [euclidean_distance(x, x_train) for x_train in self.X_train]
+
+    # Get the K-nearest neighbours(samples)
+    # we sort our distances
+    k_indices = np.argsort(distanes)[: self.k]
+
+    # labels of the samples
+    k_nearest_labels = [self.y_train[i] for i in k_indices]
+
+    # do a maturity vote-get the most common class label
+    most_common = Counter(k_nearest_labels).most_common(1)
+    return most_common[0][0]
--- KNN/KNN_test.py
+++ KNN/KNN_test.py
@@ -37,3 +37,24 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # # plotting 2d case
 # plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, edgecolor='k', s=20)
 # plt.show()
+
+from KNN import KNN
+
+clf = KNN(k=5) # use odd numbers of neighbours, k=3 for example
+
+clf.fit(X_train, y_train)
+
+predictions = clf.predict(X_test)
+
+# lets calculate the accuracy (how many of our predictions are correctkly classified with respect to y_test)
+acc = np.sum(predictions == y_test) / len(y_test)
+
+print('accuracy={}'.format(acc))
+"""
+for K=5
+accuracy=0.9666666666666667
+"""
+"""
+for k=3
+accuracy=1.0
+"""
