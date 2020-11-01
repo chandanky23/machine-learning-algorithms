e1e768bd330666d97b10cabf53ca1e3e263d7b6b
chandan yadav <chandanky.23@gmail.com>
2020-11-01 23:21:35 +0100

    Adding a regression algorithm and running the model over sample data, adding a plot of the predicted data

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -1,16 +1,31 @@
 # This alogorithm is used to predict continues data.
 # To solve this algorithm we will try and minimise our cost function using the technique called Gradient Descent.
 
+import numpy as np
+
 class LinearRegression:
 
-  def __init__(self, lr=0.001, n_iters=1000):
+  def __init__(self, lr=0.01, n_iters=1000):
     self.lr = lr
     self.n_iters = n_iters
     self.weigths = None
     self.bias = None
 
   def fit(self, X, y):
-    pass
+    # init our parameters
+    n_samples, n_features = X.shape
+    self.weigths = np.zeros(n_features)
+    self.bias = 0
+
+    for _ in range(self.n_iters):
+      y_approximate = np.dot(X, self.weigths) + self.bias
+      
+      dw = (1/n_samples) * np.dot(X.T, (y_approximate - y))
+      db = (1/n_samples) * np.sum(y_approximate-y)
+
+      self.weigths -= self.lr * dw
+      self.bias -= self.lr * db
 
   def predict(self, X):
-    pass
+    y_predicted = np.dot(X, self.weigths) + self.bias
+    return y_predicted
--- LinearRegression/linear_regression_test.py
+++ LinearRegression/linear_regression_test.py
@@ -8,9 +8,42 @@ import matplotlib.pyplot as plt
 X, y = datasets.make_regression(n_samples=1000, n_features=1, noise=20, random_state=4)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-fig = plt.figure(figsize=(8,6))
-plt.scatter(X[:, 0], y, color="b", marker="o", s=30)
-plt.show()
+# fig = plt.figure(figsize=(8,6))
+# plt.scatter(X[:, 0], y, color="b", marker="o", s=30)
+# plt.show()
 
-print(X_train.shape)
-print(y_train.shape)
+# print(X_train.shape)
+"""
+(800, 1)
+"""
+# print(y_train.shape)
+"""
+(800,)
+"""
+
+from linear_regression import LinearRegression
+
+regression = LinearRegression(lr=0.01)
+regression.fit(X_train, y_train)
+predicted = regression.predict(X_test)
+
+# calculate the mean squared error over the predicted value and the actual value ,i.e, y_test
+def mse(y_true, y_predicted):
+  return np.mean((y_true-y_predicted)**2)
+
+mse_value = mse(y_test, predicted)
+print(mse_value)
+"""
+  lr=0.01, n_iters = 1000
+  383.2488464665803
+"""
+
+# Plot this data
+y_pred_line = regression.predict(X)
+cmap = plt.get_cmap('viridis')
+fig = plt.figure(figsize=(8,6))
+m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+
+plt.plot(X, y_pred_line, color='black', linewidth=2, label="Prediction")
+plt.show()
